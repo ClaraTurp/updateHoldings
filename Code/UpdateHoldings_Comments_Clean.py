0593c67f661515b47ab363e0303ddae97e45b693
ClaraTurp <37217187+ClaraTurp@users.noreply.github.com>
2018-07-06 11:01:25 -0400

    Update UpdateHoldings_Comments_Clean.py

--- Code/UpdateHoldings_Comments_Clean.py
+++ Code/UpdateHoldings_Comments_Clean.py
@@ -1,8 +1,23 @@
-###############
-###############
+## 2018-07-06 Clara Turp, for McGill University Library.
 
+###################
+#    Functions
+###################
+
+def import_titles_inArray(myArray, filename):
+    title_list = open(filename, "r", encoding = "UTF-8", errors= "ignore")
+    for titles in title_list:
+        titles = titles.rstrip()
+        myArray.append(titles)
+
+###################
+#    Main Code   
+###################
 
 commentArray = []
+commentArray = []
+addedAlreadyCommentArray = []
+import_titles_inArray(addedAlreadyCommentArray, "EIsbnComments.txt")
 
 file = open("comments_Gobi.txt", "r", encoding = "utf-8")
 lines = file.readlines()
@@ -15,7 +30,7 @@ for line in lines:
             if len(comment[i]) > 1:
                 if comment[i][0] == "q":
                     comment[i] = comment[i][1:]
-                    if comment[i] not in commentArray:
+                    if comment[i] not in commentArray and comment[i] not in addedAlreadyCommentArray:
                         commentArray.append(comment[i])
 
 commentArray.sort()
