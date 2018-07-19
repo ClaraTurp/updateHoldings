55bc7d640db640de3f44df978d45bd902df1a405
ClaraTurp <37217187+ClaraTurp@users.noreply.github.com>
2018-07-19 16:02:59 -0400

    Update UpdateHoldings_FirstPhase.py
    Almost the same code, but re written for clarity purposes. Also includes some small improvements

--- Code/UpdateHoldings_FirstPhase.py
+++ Code/UpdateHoldings_FirstPhase.py
@@ -1,9 +1,30 @@
 ## 2018-07-06 Clara Turp, for McGill University Library.
 
+
+import csv
+import re
+import string
+
+
 ###################
 #    Functions
 ###################
 
+#Functions that read files
+def readTextFile(fileName):
+    myFile = open(fileName, "r", encoding = "utf-8", errors = "ignore")
+    reader = myFile.read()
+
+    return reader
+
+def readCsvFile(fileName):
+    myFile = open(fileName, "r", encoding = "utf-8", errors = "ignore")
+    reader = csv.reader(myFile)
+
+    return reader
+
+
+#Functions that clean strings.
 def CleanString(myString):
     import string
     cleanedString = myString.lower()
@@ -21,198 +42,168 @@ def CleanOCN(myString):
     return OCN
 
 
+#Functions that transfers text from a file to an array.
+def transferEIsbnComments(myArray):
+    isbnComments = []
+
+    reader = readTextFile("EIsbnComments.txt")
+    isbnComments = reader.split("\n")
+    for currentComment in isbnComments:
+        cleanedComment = CleanString(currentComment)
+        if cleanedComment not in myArray:
+            myArray.append(cleanedComment)
+
+
+def transferSubscriptions(ocnArray, titleArray):
+    reader = readCsvFile("subscriptions_allTitles_Final.csv")
+    for row in reader:
+        ocnArray.append(row[1])
+        titleArray.append(row[2])
+
+#Functions that treat exceptions
+def treatIsbnNotSeparatedBySlashes(myString):
+    exceptionIsbn = myString.split("$")
+    if len(exceptionIsbn) > 1:
+        for currentIsbn in exceptionIsbn:
+            if currentIsbn[0].isdigit():
+                myString = currentIsbn
+            elif currentIsbn[0] == "a":
+                myString = currentIsbn[1:]
+    return myString
+
+
+def treatCommentsWithoutSubfields(myIsbnString, isbnCommentsArray) :
+    isbnToKeep = False
+
+    isbnArray = myIsbnString.split(" ")
+    
+    if len(isbnArray[0]) > 12 :
+        if len(isbnArray[1]) < 2:
+            isbnToKeep = True
+        else:
+            for currentString in isbnArray :
+                cleanedString = CleanString(currentString)
+                if cleanedString in isbnCommentsArray:
+                    isbnToKeep = True
+
+    return isbnToKeep
+
+#Function that updates the appropriate arrays.
+def updateArray(myIsbnString, oclcNum, title, myResultArray, errorCheckingArray):
+    if myIsbnString[-1] == ";":
+        myIsbnString = myIsbnString[:-1]
+
+    if len(errorCheckingArray) < 2:
+        errorCheckingArray.append(myIsbnString)
+    else:
+        myResultArray.append([oclcNum, myIsbnString, title])            
+
+# Function to create the files where results will be uploaded.
+def createPrintFiles(filename):
+    myFile = open(filename, "w", newline = "", encoding="utf-8", errors= "ignore")
+    writer = csv.writer(myFile)
+
+    return writer
+
 ###################
 #    Main Code   
 ###################
 
-import csv
-import re
-import string
-
-isbnComments = []
 finalComments = []
-commentFile = open("EIsbnComments.txt", "r", encoding = "utf-8")
-reader = commentFile.read()
-isbnComments = reader.split("\n")
-for currentComment in isbnComments:
-    cleanedComment = CleanString(currentComment)
-    if cleanedComment not in finalComments:
-        finalComments.append(cleanedComment)
-print(finalComments)
-commentFile.close()
 
 subsOcn = []
 subs245 = []
-subscriptionFile = open("subscriptions_allTitles_Final.csv", "r", encoding = "utf-8")
-reader = csv.reader(subscriptionFile)
-for row in reader:
-    subsOcn.append(row[1])
-    subs245.append(row[2])
-
-    
-myArray = []
-myErrorArray = []
 SubscriptionArray = []
 
-with open("CurrentBatch.csv", "r", encoding = "utf-8") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        
-        OCN = CleanOCN(row[0])
-        if OCN in subsOcn:
-            SubscriptionArray.append(row)
-        elif row[2] in subs245:
-            SubscriptionArray.append(row)
-            
-        else:
-            secondLevelArray = []
-            anotherArray = []
-            secondLevelArray.append(row[0])
-            row[2] = bytes(row[2], 'utf-8').decode('utf-8','ignore')
-            
-            # row[1] = the column with the ISBN. Each ISBN is separated by \\
-            u = row[1].split("\\\\")
-            
-            for isbn in u:
-                if len(isbn)> 1 :
-                    isbn1 = isbn.split("$a")
-                    for iteration in isbn1:
-                        justIsbn = False
-                        if len(iteration) > 1 and iteration[0][0] != "$":
-                            isbn2 = iteration.split("$q")
-                            
-                            
-                            # If there is no $q
-                            if len(isbn2) == 1:
-                             # Check if ISBN were not separated by \\
-                                exceptionIsbn = isbn2[0].split("$")
-                                if len(exceptionIsbn) > 1:
-                                    for currentIsbn in exceptionIsbn:
-                                        if currentIsbn[0].isdigit():
-                                            isbn2[0] = currentIsbn
-                                        elif currentIsbn[0] == "a":
-                                            isbn2[0] = currentIsbn[1:]
-                                            
-                                # See if there are letters with regular expressions.
-                                if re.search("[a-zA-Z]", isbn2[0]):
-                                    isbn3 = isbn2[0].split(" ")
-                                    # If there are spaces, the letters are comments added without $q
-                                    if len(isbn3) > 1 :
-                                        if len(isbn3[0]) > 12 :
-                                            for currentString in isbn3 :
-                                                if currentString in isbnComments:
-                                                    if len(secondLevelArray) < 2:
-                                                        secondLevelArray.append(isbn3[0])
-                                                    else:
-                                                        anotherArray.append(row[0])
-                                                        anotherArray.append(isbn3[0])
-                                                        anotherArray.append(row[2])
+myResultArray = []
+myErrorArray = []
 
-                                                    if len(anotherArray) == 3:
-                                                        myArray.append(anotherArray)
-                                                        anotherArray = []
-                                                        
-                                    # If there are no spaces, it's an ISBN with a final x
-                                    else:
-                                        if len(isbn2[0]) > 12:
-                                            if isbn3[0][-1] == ";":
-                                                isbn3[0] = isbn3[0][:-1]
-                                                if len(secondLevelArray) < 2:
-                                                    secondLevelArray.append(isbn3[0])
-                                                else:
-                                                    anotherArray.append(row[0])
-                                                    anotherArray.append(isbn3[0])
-                                                    anotherArray.append(row[2])
-                                                if len(anotherArray) == 3:
-                                                    myArray.append(anotherArray)
-                                                    anotherArray = []
-                                            else:
-                                                if len(secondLevelArray) < 2:
-                                                    secondLevelArray.append(isbn2[0])
-                                                else:
-                                                    anotherArray.append(row[0])
-                                                    anotherArray.append(isbn2[0])
-                                                    anotherArray.append(row[2])
+transferEIsbnComments(finalComments)
+transferSubscriptions(subsOcn, subs245)
 
-                                                if len(anotherArray) == 3:
-                                                    myArray.append(anotherArray)
-                                                    anotherArray = []
+mainFileReader = readCsvFile("filename.csv")
+for row in mainFileReader:
+    OCN = CleanOCN(row[0])
+    #Remove all subscriptions:
+    if OCN in subsOcn:
+        SubscriptionArray.append(row)
+    elif row[2] in subs245:
+        SubscriptionArray.append(row)
 
-                                #if it's only numbers:
-                                elif len(isbn2[0]) > 12:
-                                    if isbn2[0][-1] == ";":
-                                        isbn2[0] = isbn2[0][:-1]
-                                        if len(secondLevelArray) < 2:
-                                            secondLevelArray.append(isbn2[0])
-                                        else:
-                                            anotherArray.append(row[0])
-                                            anotherArray.append(isbn2[0])
-                                            anotherArray.append(row[2])
-                                        if len(anotherArray) == 3:
-                                            myArray.append(anotherArray)
-                                            anotherArray = []
-                                    else:
-                                        if len(secondLevelArray) < 2:
-                                            secondLevelArray.append(isbn2[0])
-                                        else:
-                                            anotherArray.append(row[0])
-                                            anotherArray.append(isbn2[0])
-                                            anotherArray.append(row[2])
-                                        if len(anotherArray) == 3:
-                                            myArray.append(anotherArray)
-                                            anotherArray = []
+    else:
+        # errorCheckingArray allows me to find the cases when no ISBN is kept (if the array's length remains less than 2)
+        errorCheckingArray = []
+       
+        oclcNumRow = row[0]
+        errorCheckingArray.append(oclcNumRow)
 
-                                            
-                            #if there is a $q, match the comment.
+        title245Row = bytes(row[2], 'utf-8').decode('utf-8','ignore')
+
+        isbnRow = row[1].split("\\\\")
+
+        for iteration in isbnRow:
+            #Remove the empty strings. Each iteration contains MARC subfields, the ISBN ($a), and comments ($q)
+            if len(iteration) > 1:
+                #Finds the isbn rows that contain a subfield a ($a), which is for the valid ISBN
+                isbnField = iteration.split("$a")
+                for iter in isbnField:
+                    #If the length = 1, it means the ISBN is subfield z which is for invalid or canceled ISBN
+                    if len(iter) > 1 and iter[0][0] != "$":
+                        #the subfield q ($q) is for qualifying information.
+                        isbnandComment = iter.split("$q")
+                        isbn = isbnandComment[0]
+
+                        #if there is no comment ($q)
+                        if len(isbnandComment) == 1:
+                            #Deal with exceptions
+                            isbn = treatIsbnNotSeparatedBySlashes(isbn)
+                            if isbn.isdigit() and len(isbn) > 12 :
+                                updateArray(isbn, oclcNumRow, title245Row, myResultArray, errorCheckingArray)
                             else:
-                                #look if there are comment in $a in addition to $q
-                                if not isbn2[0].isdigit():
-                                    splitIsbn2 = isbn2[0].split(" ")
-                                    if splitIsbn2[0].isdigit():
-                                        isbn2[0] = splitIsbn2[0]
-                                        
-                                #Transfer arrays according to comments.        
-                                if len(isbn2[0]) > 12:
-                                    comment = CleanString(isbn2[1])
-                                    if comment in finalComments:
-                                        if len(tempArray) < 2:
-                                            tempArray.append(isbn2[0])
-                                        else:
-                                            anotherArray.append(row[0])
-                                            anotherArray.append(isbn2[0])
-                                            anotherArray.append(row[2])
+                                if " " in isbn:
+                                    isbnToKeep = treatCommentsWithoutSubfields(isbn, finalComments)
+                                    if isbnToKeep == True :
+                                        isbn = isbn.split(" ")[0]
+                                        updateArray(isbn, oclcNumRow, title245Row, myResultArray, errorCheckingArray)
+                                else:
+                                    if len(isbn) > 12:
+                                        updateArray(isbn, oclcNumRow, title245Row, myResultArray, errorCheckingArray)
 
-                                    if len(anotherArray) == 3:
-                                        myArray.append(anotherArray)
-                                        anotherArray = []
-
-            secondLevelArray.append(row[2])
-            if len(secondLevelArray) < 3 :
-                myErrorArray.append(secondLevelArray)
-            else:
-                myArray.append(secondLevelArray)
-f.close()
-
-myFile = open("1_FirstResults.csv", "w", newline = "", encoding="utf-8", errors= "ignore")
-with myFile:
-    writer = csv.writer(myFile)
-    for row in myArray:
-        writer.writerow(row)
-myFile.close()
+                        #if there is a comment ($q), match with comments from the finalComments array.
+                        else:
+                            #look if there is a comment in $a in addition to $q
+                            if not isbn.isdigit():
+                                splitIsbn = isbn.split(" ")
+                                if splitIsbn[0].isdigit():
+                                    isbn = splitIsbn[0]
+                                #Look if there is two ISBN instead of one, not separated by ///
+                                elif "$" in isbn:
+                                    splitIsbn = isbn.split("$")
+                                    if splitIsbn[0].isdigit():
+                                        isbn = splitIsbn[0]
+                                    
+                            if len(isbn) > 12:
+                                comment = CleanString(isbnandComment[1])
+                                if comment in finalComments:
+                                    updateArray(isbn, oclcNumRow, title245Row, myResultArray, errorCheckingArray)
+                            
+        errorCheckingArray.append(row[2])
+        if len(errorCheckingArray) < 3 :
+            myErrorArray.append(errorCheckingArray)
+        else:
+            myResultArray.append(errorCheckingArray)
 
 
-with open('1_ErrorFirstPhase.txt','w', encoding = "utf-8") as myErrorFile:
-    if len(myErrorArray) > 0:
-        for line in myErrorArray:
-            for instance in line:
-                myErrorFile.write(instance + "\t")
-            myErrorFile.write("\n")
-myErrorFile.close()
+writer = createPrintFiles("1_FirstResults.csv")
+for row in myResultArray:
+    writer.writerow(row)
 
-myFile = open("1_Subscriptions.csv", "w", newline = "", encoding="utf-8", errors= "ignore")
-with myFile:
-    writer = csv.writer(myFile)
+if len(myErrorArray) > 0:
+    errorWriter = createPrintFiles("1_ErrorFirstPhase.csv")
+    for line in myErrorArray:
+        errorWriter.writerow(line)
+
+if len(SubscriptionArray) > 0:
+    subsWriter = createPrintFiles("1_Subscriptions.csv")
     for row in SubscriptionArray:
-        writer.writerow(row)
-myFile.close()
-
+        subsWriter.writerow(row)
